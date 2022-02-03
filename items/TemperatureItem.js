8ce7ec757057e2a17aceee3e69a07138c3d6b255
jmago17 <32551505+jmago17@users.noreply.github.com>
2022-02-03 11:25:44 +0100

    Update TemperatureItem.js

--- items/TemperatureItem.js
+++ items/TemperatureItem.js
@@ -173,7 +173,7 @@ TemperatureItem.prototype.callBack = function(value, uuid) {
             serviceOn = false;
             console.log("Service Mode = All on for: " + this.name);
             this.setFromLoxone = true;
-            /* this.otherService
+             this.otherService
             .getCharacteristic(this.homebridge.hap.Characteristic.TargetHeatingCoolingState)
             .setValue(3, function() {
                       this.setFromLoxone = false;
@@ -181,7 +181,7 @@ TemperatureItem.prototype.callBack = function(value, uuid) {
             
             this.otherService
             .getCharacteristic(this.homebridge.hap.Characteristic.CurrentHeatingCoolingState)
-            .setValue(3);*/ 
+            .setValue(3); 
         }
          setFromHomekit = false;
     }
