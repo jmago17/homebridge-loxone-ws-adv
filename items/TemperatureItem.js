9838861f5384d81cf77de7540528eb57d0799bb2
jmago17 <32551505+jmago17@users.noreply.github.com>
2022-02-03 14:29:09 +0100

    Update TemperatureItem.js

--- items/TemperatureItem.js
+++ items/TemperatureItem.js
@@ -370,13 +370,13 @@ TemperatureItem.prototype.setTargetHeatingCoolingState = function(ValueHc, callb
         this.platform.ws.sendCommand(this.uuidAction, command);
         this.log(this.name + " Command " + command);
         
-       /* //2 hours timer
+        //2 hours timer
         
        
         
         command = "starttimer/2" + "/7200"; //Loxone expects a Value 0-6
         this.platform.ws.sendCommand(this.uuidAction, command);
-       this.log(this.name + " Command " + command);*/
+       this.log(this.name + " Command " + command);
         callback();
         serviceOn = false;
         setFromHomekit = true;
