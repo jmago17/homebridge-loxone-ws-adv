c28a5ae22e645901a902bcbddc64504a22be0e49
jmago17 <32551505+jmago17@users.noreply.github.com>
2022-02-03 14:58:02 +0100

    Update TemperatureItem.js

--- items/TemperatureItem.js
+++ items/TemperatureItem.js
@@ -525,7 +525,7 @@ TemperatureItem.prototype.setTergetTemperature = function(Value, callback) {
         var command = "settemp/" + this.currentProfile + "/" + Value; //Loxone expects a Value between 10 and 38
         this.platform.ws.sendCommand(this.uuidAction, command);
         this.log(this.name + " Command " + command);
-        command = "starttimer/"+ Value + "/7200"; //
+        command = "starttimer/"+ this.currentProfile + "/7200"; //
         this.platform.ws.sendCommand(this.uuidAction, command);
         this.log(this.name + " Command " + command);
         callback();
