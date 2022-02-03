00b9773ac3d7cb5b5b17f3a03eb94ce522389ef1
jmago17 <32551505+jmago17@users.noreply.github.com>
2022-02-03 09:06:53 +0100

    Update TemperatureItem.js

--- items/TemperatureItem.js
+++ items/TemperatureItem.js
@@ -89,6 +89,11 @@ TemperatureItem.prototype.callBack = function(value, uuid) {
             .setValue(0);*/
             serviceOn = true;
         }
+        if(value != "1") {
+            serviceOn = false;
+            console.log("Service Mode = All on for: " + this.name);
+            this.setFromLoxone = true;
+            this.otherService
     }
       
     if (serviceOn == false){
