61614b7c94fa5d68b50135a59bdb92be693dd56c
jmago17 <32551505+jmago17@users.noreply.github.com>
2022-11-13 13:00:32 +0100

    Update Alarm.js

--- items/Alarm.js
+++ items/Alarm.js
@@ -29,8 +29,8 @@ Alarm.prototype.getOtherServices = function () {
 		.on("get", this.getCurrentState.bind(this));
 	
 	otherService.getCharacteristic(this.homebridge.hap.Characteristic.SecuritySystemTargetState)
-		.on('set', this.setItemState.bind(this))
-		.on('get', this.getItemState.bind(this))
+		.on('set', AlTgState.setItemState.bind(this))
+		.on('get', AlTgState.getItemState.bind(this))
 		
 
   return otherService;
