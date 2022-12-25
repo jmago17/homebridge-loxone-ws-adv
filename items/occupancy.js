58563fa7707eca9e31008dbea42cd8a00cbfde1c
jmago17 <32551505+jmago17@users.noreply.github.com>
2022-12-25 13:35:28 +0100

    Update occupancy.js

--- items/occupancy.js
+++ items/occupancy.js
@@ -1,33 +1,11 @@
 const request = require("request");
 
-const occupan = function(widget,platform,homebridge) {
-
-    this.platform = platform;
-    this.uuidAction = widget.uuidAction; //to control a switch, use the uuidAction
-    this.stateUuid = widget.states.active; //a switch always has a state called active, which is the uuid which will receive the event to read
-    this.currentState = undefined; //will be 0 or 1 for Switch
-
-    occupan.super_.call(this, widget,platform,homebridge);
+const OccupancyItem = function(widget,platform,homebridge) {
+    OccupancyItem.super_.call(this, widget,platform,homebridge);
 };
 
-// Register a listener to be notified of changes in this items value
-occupan.prototype.initListener = function() {
-    this.platform.ws.registerListenerForUUID(this.stateUuid, this.callBack.bind(this));
-};
-
-occupan.prototype.callBack = function(value) {
-    //function that gets called by the registered ws listener
-    //console.log("Got new state for switch: " + value);
-    this.currentState = value;
-
-    //also make sure this change is directly communicated to HomeKit
-    this.otherService
-        .getCharacteristic(this.homebridge.hap.Characteristic.On)
-        .updateValue(this.currentState == '1');
-};
-
-occupan.prototype.getOtherServices = function() {
-    const otherService = new this.homebridge.hap.Service.Switch();
+OccupancyItem.prototype.getOtherServices = function() {
+    const otherService = new this.homebridge.hap.Service.Lightbulb();
 
     otherService.getCharacteristic(this.homebridge.hap.Characteristic.On)
         .on('set', this.setItemState.bind(this))
@@ -37,29 +15,4 @@ occupan.prototype.getOtherServices = function() {
     return otherService;
 };
 
-occupan.prototype.getItemState = function(callback) {
-    //returns true if currentState is 1
-    callback(undefined, this.currentState == '1');
-};
-
-occupan.prototype.onCommand = () => {
-    //function to set the command to be used for On
-    //for a switch, this is 'On', but subclasses can override this to eg Pulse
-    return 'On';
-};
-
-occupan.prototype.setItemState = function(value, callback) {
-
-    //sending new state to loxone
-    //added some logic to prevent a loop when the change because of external event captured by callback
-
-    const self = this;
-	
-    const command = (value == '1') ? this.onCommand() : 'Off';
-    this.log(`[switch] iOS - send message to ${this.name}: ${command}`);
-    this.platform.ws.sendCommand(this.uuidAction, command);
-    callback();
-
-};
-
-module.exports = occupan;
+module.exports = OccupancyItem;
