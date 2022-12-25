90f77b2a68e28bdf8fe61a588bc87331dd1d7f21
jmago17 <32551505+jmago17@users.noreply.github.com>
2022-12-25 12:17:18 +0100

    Update ItemFactory.js

--- libs/ItemFactory.js
+++ libs/ItemFactory.js
@@ -12,7 +12,7 @@ moduleexports.Dimmer = require('../items/DimmerItem.js');
 moduleexports.Jalousie = require('../items/BlindsItem.js');
 moduleexports.Pushbutton = require('../items/PushbuttonItem.js');
 
-moduleexports.occupancy = require('../items/occupa.js');
+moduleexports.PresenceDetector = require('../items/occupancy.js');
 
 
 moduleexports.Colorpicker = require('../items/ColorpickerItem.js');
