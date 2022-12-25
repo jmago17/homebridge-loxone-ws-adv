ae170c102014717f14b7f4ccdabf59a37f870055
jmago17 <32551505+jmago17@users.noreply.github.com>
2022-12-25 12:08:29 +0100

    Update occupancy.js

--- items/occupancy.js
+++ items/occupancy.js
@@ -1,151 +1,65 @@
-let Characteristic, Service
+const request = require("request");
 
-class OccupancySensor {
-	constructor(network, config, platform) {
+const SwitchItem = function(widget,platform,homebridge) {
 
-		Service = platform.api.hap.Service
-		Characteristic = platform.api.hap.Characteristic
-		this.network = network
-		this.log = platform.log
-		this.api = platform.api
-		this.platformDevices = platform.devices
-		this.interval = platform.interval
-		this.threshold = !config.threshold && config.threshold !== 0 ? platform.threshold : config.threshold
-		this.mac = config.mac ? config.mac.toLowerCase() : null
-		this.ip = config.ip
-		this.hostname = config.hostname ? config.hostname.toLowerCase() : null
-		this.name = config.name
-		this.model = 'ARP-network-scanner'
-		this.serial = this.mac || this.ip || this.hostname
-		this.manufacturer = '@nitaybz'
-		this.displayName = this.name
+    this.platform = platform;
+    this.uuidAction = widget.uuidAction; //to control a switch, use the uuidAction
+    this.stateUuid = widget.states.active; //a switch always has a state called active, which is the uuid which will receive the event to read
+    this.currentState = undefined; //will be 0 or 1 for Switch
 
-		this.thresholdTimer = null
+    SwitchItem.super_.call(this, widget,platform,homebridge);
+};
 
-		if (!this.serial) {
-			this.log(`Can't initiate ${this.name} device without mac address, ip address or hostname`)
-			this.log(`Please change your config`)
-			return
-		}
+// Register a listener to be notified of changes in this items value
+SwitchItem.prototype.initListener = function() {
+    this.platform.ws.registerListenerForUUID(this.stateUuid, this.callBack.bind(this));
+};
 
-		if (typeof this.serial !== 'string') {
-			this.log(`Wrong mac/ip address/hostname format`)
-			this.log(`Please adjust your config to include proper ip address (10.0.0.x), mac address (3e:34:ae:87:f1:cc) or hostname (joes-iphone.local)`)
-			return
-		}
+SwitchItem.prototype.callBack = function(value) {
+    //function that gets called by the registered ws listener
+    //console.log("Got new state for switch: " + value);
+    this.currentState = value;
 
-		this.UUID = this.api.hap.uuid.generate(this.serial)
-		this.accessory = platform.accessories.find(accessory => accessory.UUID === this.UUID)
+    //also make sure this change is directly communicated to HomeKit
+    this.otherService
+        .getCharacteristic(this.homebridge.hap.Characteristic.On)
+        .updateValue(this.currentState == '1');
+};
 
-		if (!this.accessory) {
-			this.log(`Creating New ${platform.PLATFORM_NAME} Accessory for ${this.name}`)
-			this.accessory = new this.api.platformAccessory(this.name, this.UUID)
-			this.accessory.context.serial = this.serial
+SwitchItem.prototype.getOtherServices = function() {
+    const otherService = new this.homebridge.hap.Service.Switch();
 
-			platform.accessories.push(this.accessory)
-			// register the accessory
-			this.api.registerPlatformAccessories(platform.PLUGIN_NAME, platform.PLATFORM_NAME, [this.accessory])
-		}
+    otherService.getCharacteristic(this.homebridge.hap.Characteristic.On)
+        .on('set', this.setItemState.bind(this))
+        .on('get', this.getItemState.bind(this))
+        .updateValue(this.currentState == '1');
 
+    return otherService;
+};
 
-		this.isDetected = 0
+SwitchItem.prototype.getItemState = function(callback) {
+    //returns true if currentState is 1
+    callback(undefined, this.currentState == '1');
+};
 
-		let informationService = this.accessory.getService(Service.AccessoryInformation)
+SwitchItem.prototype.onCommand = () => {
+    //function to set the command to be used for On
+    //for a switch, this is 'On', but subclasses can override this to eg Pulse
+    return 'On';
+};
 
-		if (!informationService)
-			informationService = this.accessory.addService(Service.AccessoryInformation)
+SwitchItem.prototype.setItemState = function(value, callback) {
 
-		informationService
-			.setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
-			.setCharacteristic(Characteristic.Model, this.model)
-			.setCharacteristic(Characteristic.SerialNumber, this.serial)
+    //sending new state to loxone
+    //added some logic to prevent a loop when the change because of external event captured by callback
 
-		if (config.anyone)
-			this.addAnyoneSensor()
-		else
-			this.addOccupancySensor()
-
-	}
-
-	addOccupancySensor() {
-		this.log.easyDebug(`Adding "${this.name}" Occupancy Sensor Service`)
-		this.OccupancySensorService = this.accessory.getService(Service.OccupancySensor)
-		if (!this.OccupancySensorService)
-			this.OccupancySensorService = this.accessory.addService(Service.OccupancySensor, this.name, 'netSensor')
-
-		this.OccupancySensorService.getCharacteristic(Characteristic.OccupancyDetected)
-			.on('get', (callback) => callback(null, this.isDetected))
-			.updateValue(this.isDetected)
-
-		const listenTo = this.mac ? `mac:${this.mac}` : this.ip ? `ip:${this.ip}` : `hostname:${this.hostname}`
-		this.log.easyDebug(`[${this.name}] - Listening to ${listenTo}`)
-
-		this.network.on(`net-connected:${listenTo}`, (device) =>
-				this.setDetected(1, device)
-		);
-		this.network.on(`net-disconnected:${listenTo}`, (device) =>
-				this.setDetected(0, device)
-		);
-		
-	}
-
-	setDetected(isDetected, device) {
-		clearTimeout(this.thresholdTimer)
-		if (isDetected && !this.isDetected) {
-			this.log(`[${this.name}] - connected to the network (mac: ${device.mac} | ip:${device.ip} | hostname:${device.name})`)
-			this.isDetected = 1
-			this.OccupancySensorService
-				.getCharacteristic(Characteristic.OccupancyDetected)
-				.updateValue(1)
-			return
-		}
-
-		this.thresholdTimer = setTimeout(() => {
-			this.log(`[${this.name}] - disconnected from the network (mac: ${device.mac} | ip:${device.ip} | hostname:${device.name})`)
-			this.isDetected = 0
-			this.OccupancySensorService
-				.getCharacteristic(Characteristic.OccupancyDetected)
-				.updateValue(0)
-
-		}, this.threshold * 60 * 1000)
-	}
-
-
-	addAnyoneSensor() {
-		this.log.easyDebug(`Adding "${this.name}" Occupancy Sensor Service`)
-		this.OccupancySensorService = this.accessory.getService(Service.OccupancySensor)
-		if (!this.OccupancySensorService)
-			this.OccupancySensorService = this.accessory.addService(Service.OccupancySensor, this.name, 'anyoneSensor')
-
-		this.updateAnyone()
-
-		this.OccupancySensorService.getCharacteristic(Characteristic.OccupancyDetected)
-			.on('get', (callback) => callback(null, this.isDetected))
-			.updateValue(this.isDetected)
-
-		setInterval(() => {
-			this.updateAnyone()
-		}, this.interval)
-	}
+    const self = this;
 	
-	updateAnyone() {
+    const command = (value == '1') ? this.onCommand() : 'Off';
+    this.log(`[switch] iOS - send message to ${this.name}: ${command}`);
+    this.platform.ws.sendCommand(this.uuidAction, command);
+    callback();
 
-		// check if any accessory isDetected ( === 1 )
-		const isDetected = this.platformDevices.find(pDevice => pDevice.isDetected)
-		if (isDetected && !this.isDetected) {
-			this.log(`[${this.name}] - Someone connected to the network`)
-			this.isDetected = 1
-			this.OccupancySensorService
-				.getCharacteristic(Characteristic.OccupancyDetected)
-				.updateValue(1)
-		} else if (!isDetected && this.isDetected) {
-			this.log(`[${this.name}] - No one is connected to the network`)
-			this.isDetected = 0
-			this.OccupancySensorService
-				.getCharacteristic(Characteristic.OccupancyDetected)
-				.updateValue(0)
-		}
-	}
-}
+};
 
-module.exports = OccupancySensor
+module.exports = SwitchItem;
