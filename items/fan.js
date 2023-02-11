c3d44e2301e3c63370293ed376bd6e6848a0b75b
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-02-11 13:55:16 +0100

    Update fan.js

--- items/fan.js
+++ items/fan.js
@@ -1,5 +1,24 @@
 const FanItem = function(widget,platform,homebridge) {
-    FanItem.super_.call(this, widget,platform,homebridge);
+    this.platform = platform;
+    this.widget =  widget;
+    this.homebridge = homebridge;
+    this.log = this.platform.log;
+    this.name = widget.name;
+    this.UUID = homebridge.hap.uuid.generate(String(widget.uuidAction));
+    
+    // provide explicit UUID to prevent automatic UUID generation by homebridge (which would fail because of possibly equal item name)
+    this.uuid_base = this.UUID;
+
+    //other variables used by child classes
+    this.setFromLoxone = false;
+    
+	// console.log("Generating new homebridge accessory '" + this.name + "' with UUID: " + this.UUID + " from accessory with ID: " + widget.uuidAction);
+
+    //Add as ACCESSORY (parent class)
+    //AbstractItem.super_.call(this, this.name, this.UUID);
+    new homebridge.platformAccessory(this.name, this.UUID);
+
+};
 };
 
 FanItem.prototype.getOtherServices = function() {
