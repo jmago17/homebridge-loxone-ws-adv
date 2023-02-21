b67d6cd45b5dc8405daa58f96868d36c9f5bd7c8
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-02-21 16:12:38 +0100

    Update IRCV2Item.js

--- items/IRCV2Item.js
+++ items/IRCV2Item.js
@@ -69,12 +69,12 @@ IRCV2Item.prototype.callBack = function(value, uuid) {
                  .getCharacteristic(this.homebridge.hap.Characteristic.HeatingThresholdTemperature)
        		 .setValue(this.heatingTargetTemp, function() {
                   this.setFromLoxone = false;
-                  }.bind(this);
+                  }.bind(this));
 			   this.otherService
         .getCharacteristic(this.homebridge.hap.Characteristic.CoolingThresholdTemperature)
         .setValue(this.coolingTargetTemp, function() {
                   this.setFromLoxone = false;
-                  }.bind(this);
+                  }.bind(this));
 		     return;
 	     case 1:
               this.economymode = false;
