04bf9c7d89df578d74a9f3c4de8e939166df7859
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-02-21 21:06:53 +0100

    Update IRCV2Item.js

--- items/IRCV2Item.js
+++ items/IRCV2Item.js
@@ -501,7 +501,7 @@ IRCV2Item.prototype.setHeatingTemperature = function(Value, callback) {
    	
  		   //console.log("date now in seconds" + datenow.getTime())
  		  let timer = Math.round((Math.abs(datenow - date2009))/1000 + 6000);
- 		  var command = "override/2/"+ timer; 
+ 		  var command = "override/1/"+ timer; 
 		this.platform.ws.sendCommand(this.uuidAction, command);
        		 this.log(this.name + " Command " + command);
 	}
