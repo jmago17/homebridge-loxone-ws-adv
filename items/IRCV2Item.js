3d2188de9fe0be30839063cd790b3352994d8ee6
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-02-21 10:38:33 +0100

    Update IRCV2Item.js

--- items/IRCV2Item.js
+++ items/IRCV2Item.js
@@ -307,9 +307,10 @@ IRCV2Item.prototype.setTargetHeatingCoolingState = function(ValueHc, callback) {
     
     
     //getting seconds since 2009
-    var date2009 = new Date("2010-01-01 00:00:00");
+    var date2009 = new Date("2009-01-01 00:00:00");
+    console.log("date 2010 in seconds" + date2009.getime())
     var datenow = new Date();
-    let timer = Math.abs(datenow.getTime() - date2009.getTime()) + 8935;
+    let timer = Math.abs(datenow - date2009) + 8935;
     
     var self = this;
     
