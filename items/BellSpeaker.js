649b34846a134ad780fda668bed2777bdf2deeac
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-02-22 21:29:53 +0100

    Update BellSpeaker.js

--- items/BellSpeaker.js
+++ items/BellSpeaker.js
@@ -1,21 +1,21 @@
 const request = require("request");
 
-const bellSpeaker = function(widget,platform,homebridge) {
+const SpeakerItem = function(widget,platform,homebridge) {
 
     this.platform = platform;
     this.uuidAction = widget.uuidAction; //to control a switch, use the uuidAction
     this.stateUuid = widget.states.active; //a switch always has a state called active, which is the uuid which will receive the event to read
     this.mute = undefined; //will be 0 or 1 for speaker
 
-    bellSpeaker.super_.call(this, widget,platform,homebridge);
+    SpeakerItem.super_.call(this, widget,platform,homebridge);
 };
 
 // Register a listener to be notified of changes in this items value
-bellSpeaker.prototype.initListener = function() {
+SpeakerItem.prototype.initListener = function() {
     this.platform.ws.registerListenerForUUID(this.stateUuid, this.callBack.bind(this));
 };
 
-bellSpeaker.prototype.callBack = function(value) {
+SpeakerItem.prototype.callBack = function(value) {
     //function that gets called by the registered ws listener
     //console.log("Got new state for switch: " + value);
     this.currentState = value;
@@ -26,7 +26,7 @@ bellSpeaker.prototype.callBack = function(value) {
         .updateValue(this.currentState == '1');
 };
 
-bellSpeaker.prototype.getOtherServices = function() {
+SpeakerItem.prototype.getOtherServices = function() {
     const otherService = new this.homebridge.hap.Service.Speaker();
 
     otherService.getCharacteristic(this.homebridge.hap.Characteristic.Mute)
@@ -37,18 +37,18 @@ bellSpeaker.prototype.getOtherServices = function() {
     return otherService;
 };
 
-bellSpeaker.prototype.getItemState = function(callback) {
+SpeakerItem.prototype.getItemState = function(callback) {
     //returns true if currentState is 1
     callback(undefined, this.currentState == '1');
 };
 
-bellSpeaker.prototype.onCommand = () => {
+SpeakerItem.prototype.onCommand = () => {
     //function to set the command to be used for On
     //for a switch, this is 'On', but subclasses can override this to eg Pulse
     return 'On';
 };
 
-bellSpeaker.prototype.setItemState = function(value, callback) {
+SpeakerItem.prototype.setItemState = function(value, callback) {
 
     //sending new state to loxone
     //added some logic to prevent a loop when the change because of external event captured by callback
@@ -62,4 +62,4 @@ bellSpeaker.prototype.setItemState = function(value, callback) {
 
 };
 
-module.exports = bellSpeaker;
+module.exports = SpeakerItem;
