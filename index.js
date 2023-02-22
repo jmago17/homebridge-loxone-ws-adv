5375c5d4336c743afe073864518e6a12d5ca5f36
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-02-22 21:04:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ module.exports = homebridge => {
         Utility.addSupportTo(ItemFactory.HumiditySensor, ItemFactory.AbstractItem);
         Utility.addSupportTo(ItemFactory.MotionSensor, ItemFactory.AbstractItem);
 	Utility.addSupportTo(ItemFactory.Fan, ItemFactory.AbstractItem);
-	Utility.addSupportTo(ItemFactory.Speaker, ItemFactory.AbstractItem);
+	Utility.addSupportTo(ItemFactory.speakerBell, ItemFactory.AbstractItem);
         Utility.addSupportTo(ItemFactory.ContactSensor, ItemFactory.AbstractItem);
         Utility.addSupportTo(ItemFactory.LightSensor, ItemFactory.AbstractItem);
         Utility.addSupportTo(ItemFactory.Dimmer, ItemFactory.AbstractItem);
