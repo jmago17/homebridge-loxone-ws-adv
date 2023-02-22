636167f92a52b32ea1c463008182e9a799f764e8
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-02-22 20:20:34 +0100

    Update Fan.js

--- items/Fan.js
+++ items/Fan.js
@@ -1,65 +1,79 @@
 const request = require("request");
 
-const SwitchItem = function(widget,platform,homebridge) {
+const Fan = function(widget,platform,homebridge) {
 
     this.platform = platform;
     this.uuidAction = widget.uuidAction; //to control a switch, use the uuidAction
-    this.stateUuid = widget.states.active; //a switch always has a state called active, which is the uuid which will receive the event to read
     this.currentState = undefined; //will be 0 or 1 for Switch
 
-    SwitchItem.super_.call(this, widget,platform,homebridge);
+    Fan.super_.call(this, widget,platform,homebridge);
 };
 
 // Register a listener to be notified of changes in this items value
-SwitchItem.prototype.initListener = function() {
-    this.platform.ws.registerListenerForUUID(this.stateUuid, this.callBack.bind(this));
+Fan.prototype.initListener = function() {
+    //this.platform.ws.registerListenerForUUID(this.stateUuid, this.callBack.bind(this));
+    this.platform.ws.registerListenerForUUID(this.uuidAction, this.callBack.bind(this));
 };
 
-SwitchItem.prototype.callBack = function(value) {
+Fan.prototype.callBack = function(value) {
     //function that gets called by the registered ws listener
-    //console.log("Got new state for switch: " + value);
-    this.currentState = value;
+    if (value == -1) {
+        //console.log("Got new state for Timed Switch: On");
+    } else if (value == 0) {
+        //console.log("Got new state for Timed Switch: Off");
+    } else if (value > 0) {
+        //console.log("Got new state for Timed Switch: Countdown " + value + "s");
+    }
+    
+    this.currentState = (value !== 0);
 
-    //also make sure this change is directly communicated to HomeKit
+    //console.log('set currentState to: ' + this.currentState)
+
     this.otherService
         .getCharacteristic(this.homebridge.hap.Characteristic.On)
-        .updateValue(this.currentState == '1');
+        .updateValue(this.currentState);
 };
 
-SwitchItem.prototype.getOtherServices = function() {
+Fan.prototype.getOtherServices = function() {
     const otherService = new this.homebridge.hap.Service.Switch();
 
     otherService.getCharacteristic(this.homebridge.hap.Characteristic.On)
         .on('set', this.setItemState.bind(this))
         .on('get', this.getItemState.bind(this))
-        .updateValue(this.currentState == '1');
+        //.updateValue(this.currentState == '1');
 
     return otherService;
 };
 
-SwitchItem.prototype.getItemState = function(callback) {
+Fan.prototype.getItemState = function(callback) {
     //returns true if currentState is 1
-    callback(undefined, this.currentState == '1');
+    callback(undefined, this.currentState);
 };
 
-SwitchItem.prototype.onCommand = () => {
-    //function to set the command to be used for On
-    //for a switch, this is 'On', but subclasses can override this to eg Pulse
-    return 'On';
-};
-
-SwitchItem.prototype.setItemState = function(value, callback) {
+Fan.prototype.setItemState = function(value, callback) {
 
     //sending new state to loxone
     //added some logic to prevent a loop when the change because of external event captured by callback
 
-    const self = this;
-	
-    const command = (value == '1') ? this.onCommand() : 'Off';
-    this.log(`[switch] iOS - send message to ${this.name}: ${command}`);
+
+
+    let command = 0;
+    if (value == true) {
+        this.log('perm on ***');
+        command = true;//-1; // perm on
+    } else {
+        this.log('off ***');
+        command = false;//0; // off
+    }
+
+    //this.log('setItemState value: ' + value);
+    //this.log('setItemState command: ' + command);
+
+    this.log(`[timedswitch] iOS - send message to ${this.name}: ${command}`);
     this.platform.ws.sendCommand(this.uuidAction, command);
     callback();
 
 };
 
-module.exports = SwitchItem;
+module.exports = Fan;
+
