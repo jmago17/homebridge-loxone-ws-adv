9a62663c078b2ac32d738fc16f1f043d7c0e06c5
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-02-22 21:09:07 +0100

    Update ItemFactory.js

--- libs/ItemFactory.js
+++ libs/ItemFactory.js
@@ -150,7 +150,10 @@ moduleexports.Factory.prototype.checkCustomAttrs = (factory, itemId, platform, c
         } else {
             item.type = "TimedSwitch";
         }
-
+     if (item.type === "Switch") {
+            if(item.name.indexOf('BellSpeaker')){
+               item.type = "speakerBell";}}
+         
     } else if (catList[item.cat] !== undefined && catList[item.cat].image === "00000000-0000-0002-2000000000000000.svg") {
         //this is the lightbulb image, which means that this is a lightning control
         if (item.type === "Switch") {
