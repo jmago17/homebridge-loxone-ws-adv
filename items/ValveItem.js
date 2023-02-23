8a3135b8a4646ff2fad15c1ca1170cd7afb3ffc8
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-02-23 20:32:32 +0100

    Update ValveItem.js

--- items/ValveItem.js
+++ items/ValveItem.js
@@ -46,7 +46,7 @@ ValveItem.prototype.getOtherServices = function () {
 };
 
 ValveItem.prototype.setItemState = function (value, callback) {
-    this.log(`[${this.item}] - send message to ${this.name}:` + value);
+    this.log(`[${this.item}] ${this.name} ${this.uuidAction} - send message to ${this.name}:` + value);
     if (this.name == 'Programa Termo'){
         if (value == 1){ 
          this.log(`[${this.item}] ${this.name}  - send message to ${this.name}:` + value);
