afdf6980a09e48cbf2609fff249fbef35f010a78
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-02-23 22:10:33 +0100

    Update ValveItem.js

--- items/ValveItem.js
+++ items/ValveItem.js
@@ -5,7 +5,7 @@ const ValveItem = function(widget, platform, homebridge) {
     this.uuidAction = widget.uuidAction; //to control a switch, use the uuidAction
     if (this.uuidAction == '1a9bc5a7-008b-05ef-ffff8795bbcbc15c') {
         this.stateUuid = widget.states.value;
-        this.stateUuid = widget.states.override;
+        this.stateOverride = widget.states.override;
     } else {
         this.stateUuid = widget.states.active; //a switch always has a state called active, which is the uuid which will receive the event to read
     }
@@ -18,15 +18,22 @@ const ValveItem = function(widget, platform, homebridge) {
 // Register a listener to be notified of changes in this items value
 ValveItem.prototype.initListener = function() {
     this.platform.ws.registerListenerForUUID(this.stateUuid, this.callBack.bind(this));
+    if (this.uuidAction == '1a9bc5a7-008b-05ef-ffff8795bbcbc15c') {
+        this.platform.ws.registerListenerForUUID(this.stateOverride, this.callBack.bind(this));
+    }
 };
 
 ValveItem.prototype.callBack = function(value) {
-    //console.log("Got new state for sprinkler: " + value);
+    console.log("Funtion value " + value + " " + uuid);
+    if (this.stateUuid == uuid) {
+        this.currentState = value;
 
-    this.currentState = value;
-
-    this.otherService.getCharacteristic(Characteristic.Active).updateValue(this.currentState == '1');
-    this.otherService.getCharacteristic(Characteristic.InUse).updateValue(this.currentState == '1');
+        this.otherService.getCharacteristic(Characteristic.Active).updateValue(this.currentState == '1');
+        this.otherService.getCharacteristic(Characteristic.InUse).updateValue(this.currentState == '1');
+    }
+    if (this.stateOverride == uuid) {
+        this.override = value;
+    }
 };
 
 ValveItem.prototype.getOtherServices = function() {
@@ -47,26 +54,26 @@ ValveItem.prototype.getOtherServices = function() {
 };
 
 ValveItem.prototype.setItemState = function(value, callback) {
-        let command = "command";
-        this.log(`[${this.item}] ${this.name} ${this.uuidAction} - send message to ${this.name}:` + value + command);
-        if (this.uuidAction == '1a9bc5a7-008b-05ef-ffff8795bbcbc15c') {
-            if (value == 0) {
-                command = 'startOverride/1/7200';
-            } else if (this.override == 0) {
-                var now = new Date();
-                var tomorrow = new Date();
-                tomorrow.setDate(datenow.getDate() + 1)
-                tomorrow.setHours(0, 0);
-                let timer = Math.round((Math.abs(tomorrow - now)) / 1000);
-                command = 'startOverride/0/timer';
-            } else {
-                command = "stopOverride";
-            }
+    let command = "command";
+    this.log(`[${this.item}] ${this.name} ${this.uuidAction} - send message to ${this.name}:` + value + command);
+    if (this.uuidAction == '1a9bc5a7-008b-05ef-ffff8795bbcbc15c') {
+        if (value == 0) {
+            command = 'startOverride/1/7200';
+        } else if (this.override == 0) {
+            var now = new Date();
+            var tomorrow = new Date();
+            tomorrow.setDate(datenow.getDate() + 1)
+            tomorrow.setHours(0, 0);
+            let timer = Math.round((Math.abs(tomorrow - now)) / 1000);
+            command = 'startOverride/0/timer';
         } else {
-            command = (value == 1) ? 'On' : 'Off';
+            command = "stopOverride";
         }
-        this.log(`[${this.item}] - send message to ${this.name}: ${command}`);
-        this.platform.ws.sendCommand(this.uuidAction, command);
-        callback();;
+    } else {
+        command = (value == 1) ? 'On' : 'Off';
+    }
+    this.log(`[${this.item}] - send message to ${this.name}: ${command}`);
+    this.platform.ws.sendCommand(this.uuidAction, command);
+    callback();;
 }
 module.exports = ValveItem;
