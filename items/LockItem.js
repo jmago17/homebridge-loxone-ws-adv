88e8decde887a999747887b12b7e91d1e4aca401
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-02-24 17:32:47 +0100

    Update LockItem.js

--- items/LockItem.js
+++ items/LockItem.js
@@ -22,11 +22,10 @@ LockItem.prototype.callBack = function(value) {
       if (value == 0) {
         console.log("Got new state for door: off / closed");
           this.currentState = 1;
+          this.otherService.setCharacteristic(Characteristic.LockTargetState, Characteristic.LockTargetState.SECURED)
     } else {
         console.log("opened");
-          this.currentState = 0;
-        this.otherService.setCharacteristic(Characteristic.LockTargetState, Characteristic.LockTargetState.SECURED)
-   
+          this.currentState = 0;   
     }
 
     //console.log('set currentState to: ' + this.currentState)
