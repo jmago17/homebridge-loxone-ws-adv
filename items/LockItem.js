154dfb1c434dc5768451b670e033f37df5779921
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-02-24 11:06:28 +0100

    Update LockItem.js

--- items/LockItem.js
+++ items/LockItem.js
@@ -1,70 +1,82 @@
-const LockItem = function (widget, platform, homebridge) {
+const request = require("request");
+
+const LockItem = function(widget,platform,homebridge) {
+
     this.platform = platform;
     this.uuidAction = widget.uuidAction; //to control a switch, use the uuidAction
-    this.stateUuid = widget.states.active; //a switch always has a state called active, which is the uuid which will receive the event to read
-    this.currentState = widget.states.position; //will be 0 or 1 for Switch
-       
-    LockItem.super_.call(this, widget, platform, homebridge);
-    var setFromLoxone = true;
+    this.currentState = undefined; //will be 0 or 1 for Switch
+
+    LockItem.super_.call(this, widget,platform,homebridge);
 };
 
 // Register a listener to be notified of changes in this items value
-LockItem.prototype.initListener = function () {
-    this.platform.ws.registerListenerForUUID(this.currentState, this.callBack.bind(this));
-    this.platform.ws.registerListenerForUUID(this.stateUuid, this.callBack.bind(this));
-     console.log('listeners lock item');
+LockSwitchItem.prototype.initListener = function() {
+    //this.platform.ws.registerListenerForUUID(this.stateUuid, this.callBack.bind(this));
+    this.platform.ws.registerListenerForUUID(this.uuidAction, this.callBack.bind(this));
 };
 
-LockItem.prototype.callBack = function (value, uuid) {
-    console.log("Got new state for lock: " + value + " " + uuid);
-    this.setFromLoxone = true;
-    if ( this.currentState = uuid){
-         console.log("Got new state for lock: " + value + " " + uuid);
-        let new_doorstate = value;
-        if (value == 1) {
-            this.otherService.getCharacteristic(Characteristic.LockTargetState).setValue('1');
-            console.log('OPENed');
-        } else { this.otherService.getCharacteristic(Characteristic.LockTargetState).setValue('0');
-             console.log('closed');
-               }    
+LockSwitchItem.prototype.callBack = function(value) {
+    //function that gets called by the registered ws listener
+    if (value == -1) {
+        //console.log("Got new state for Timed Switch: On");
+    } else if (value == 0) {
+        //console.log("Got new state for Timed Switch: Off");
+    } else if (value > 0) {
+        //console.log("Got new state for Timed Switch: Countdown " + value + "s");
     }
-        
-    if ( this.stateUuid = uuid){  
-         let new_targetdoorstate = value;
-         if (value == 1 || value == 0) {
-              console.log('unlocked');
-              this.otherService.getCharacteristic(Characteristic.LockCurrentState).setValue('0');
-         } else { 
-            this.otherService.getCharacteristic(Characteristic.LockCurrentState).setValue('1');
-             console.log('locked');
-         }
-    }
-   setFromLoxone = false;  
     
+    this.currentState = (value !== 0);
+
+    //console.log('set currentState to: ' + this.currentState)
+
+    this.otherService
+        .getCharacteristic(this.homebridge.hap.Characteristic.SECURED)
+        .updateValue(this.currentState);
 };
 
-LockItem.prototype.getOtherServices = function () {
+LockSwitchItem.prototype.getOtherServices = function() {
     const otherService = new this.homebridge.hap.Service.LockMechanism();
 
-    otherService.setCharacteristic(Characteristic.LockCurrentState, Characteristic.LockCurrentState.SECURED);
-    otherService.setCharacteristic(Characteristic.LockTargetState, Characteristic.LockTargetState.SECURED);
-
-    otherService.getCharacteristic(Characteristic.LockTargetState)
+    otherService.getCharacteristic(this.homebridge.hap.LockCurrentState.SECURED)
         .on('set', this.setItemState.bind(this))
-
+        .on('get', this.getItemState.bind(this))
+        //.updateValue(this.currentState == '1');
+    otherService.getCharacteristic(this.homebridge.hap.LockTargetState.SECURED)
+        .on('set', this.setItemState.bind(this))
+        .on('get', this.getItemState.bind(this))
+        //.updateValue(this.currentState == '1');
     return otherService;
 };
 
+LockSwitchItem.prototype.getItemState = function(callback) {
+    //returns true if currentState is 1
+    callback(undefined, this.currentState);
+};
+
+LockSwitchItem.prototype.setItemState = function(value, callback) {
+
+    //sending new state to loxone
+    //added some logic to prevent a loop when the change because of external event captured by callback
 
 
-LockItem.prototype.setItemState = function (value, callback) {
-    if(!setFromLoxone) {
-    const command = (value != '1') ? 'open' : 'close';
-    this.log(`[Lock] - send message to ${this.name}: ${command}`);
+
+    let command = 0;
+    if (value == true) {
+        //this.log('perm on ***');
+        command = 'Pulse';//-1; // perm on
+    } else {
+        //this.log('off ***');
+        command = 'Off';//0; // off
+    }
+
+    //this.log('setItemState value: ' + value);
+    //this.log('setItemState command: ' + command);
+
+    this.log(`[timedswitch] iOS - send message to ${this.name}: ${command}`);
     this.platform.ws.sendCommand(this.uuidAction, command);
     callback();
-    }
-    setFromLoxone = false;
+
 };
 
 module.exports = LockItem;
+
