7f5337c7140b59749618576a06c5243ade59480a
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-02-28 21:39:47 +0100

    Update AbstractItem.js

--- items/AbstractItem.js
+++ items/AbstractItem.js
@@ -25,11 +25,11 @@ const AbstractItem = function(widget,platform,homebridge) {
 AbstractItem.prototype.getServices = function() {
     this.informationService = this.getInformationServices();
     this.otherService = this.getOtherServices();
-    console.log("before                if);
+    console.log("before                if");
     if (this.otherService.testCharacteristic(Characteristic.ColorTemperature) && this.otherService.testCharacteristic(Characteristic.Brightness))  {
             this.adaptiveLightingController = new this.homebridge.hap.AdaptiveLightingController(newService);
         }
-    console.log("after                if);
+    console.log("after                if");
     this.initListener();	
     return [this.informationService, this.otherService];
 	
