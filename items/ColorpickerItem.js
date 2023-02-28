707da1d9c847dafe794c1dd8b2eb887aebd7b321
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-02-28 23:29:41 +0100

    Update ColorpickerItem.js

--- items/ColorpickerItem.js
+++ items/ColorpickerItem.js
@@ -201,7 +201,7 @@ ColorItem.prototype.setColorTemperature = function(value, callback) {
     //compose hsv string
     this.colorTemperature= parseInt(value);
     this.power = this.brightness > 0;
-    const command = `temp(${this.brightness},{this.colorTemperature})`; //  temp({brightness},{temperature})
+    const command = `temp(${this.brightness},${this.colorTemperature})`; //  temp({brightness},{temperature})
     this.log(`[color] iOS - send message to ${this.name}: ${command} uuid: ${this.uuid}`);
     this.platform.ws.sendCommand(this.uuidAction, command);
     callback();
