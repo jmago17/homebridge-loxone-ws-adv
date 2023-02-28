4ef2656c8e6abd84f890338df3949c0d68c84717
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-02-28 12:08:00 +0100

    Update AbstractItem.js

--- items/AbstractItem.js
+++ items/AbstractItem.js
@@ -25,7 +25,7 @@ const AbstractItem = function(widget,platform,homebridge) {
 AbstractItem.prototype.getServices = function() {
     this.informationService = this.getInformationServices();
     this.otherService = this.getOtherServices();
-    this.otherController = this.getOtherControllers();	
+  //  this.otherController = this.getOtherControllers();	
     this.initListener();
     return [this.informationService, this.otherService];
 };
