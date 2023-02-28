98b6d3256678e7934f45fde211834feebe3a80f8
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-02-28 21:29:01 +0100

    Update AbstractItem.js

--- items/AbstractItem.js
+++ items/AbstractItem.js
@@ -48,7 +48,7 @@ AbstractItem.prototype.getInformationServices = function() {
     return informationService;
 };
 
-AbstractItem.prototype.getControllers: function() {
+AbstractItem.prototype.getControllers = function() {
         if (!this.adaptiveLightingController) {
             return [];
         } else {
