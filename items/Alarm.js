cbd35dde3a6723c7352486b4f3e53df082e0810d
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-03-01 11:45:13 +0100

    Update Alarm.js

--- items/Alarm.js
+++ items/Alarm.js
@@ -1,138 +1,71 @@
-"use strict";
-
-var request = require("request");
-
-var Alarm = function(widget, platform, homebridge) {
+var Alarm = function (widget, platform, homebridge) {
+    Characteristic = homebridge.hap.Characteristic;
 
     this.platform = platform;
     this.uuidAction = widget.uuidAction;
     this.stateUuid = widget.states.armed;
-    this.stateLevel = widget.states.level;
-    this.stateDisableMove = widget.states.disabledMove;
+    this.stateUuidAlarm = widget.states.level;
+    this.alarmsystem_method = platform.alarmsystem_method;
+    this.alarmsystem_trigger = platform.alarmsystem_trigger;
 
-
-
-    
-    this.armedState = 0;
-    this.triggeredState = 0;
     this.targetState = 0;
-
+    this.alarmlevel = 0;
 
     Alarm.super_.call(this, widget, platform, homebridge);
 };
 
-Alarm.prototype.initListener = function() {
+Alarm.prototype.initListener = function () {
     this.platform.ws.registerListenerForUUID(this.stateUuid, this.callBack.bind(this));
-    this.platform.ws.registerListenerForUUID(this.stateLevel, this.callBack.bind(this));
-    this.platform.ws.registerListenerForUUID(this.stateDisableMove, this.callBack.bind(this));
+    this.platform.ws.registerListenerForUUID(this.stateUuidAlarm, this.alarmTriggered.bind(this));
 };
 
-Alarm.prototype.callBack = function(value, uuid) {
-    // console.log("Funtion value " + value + " " + uuid);
-    if (this.stateLevel == uuid) {
-        console.log("stateLevel " + value + " " + uuid);
-        if (this.triggeredState > 0) {
-            this.armedtState = 4;                       
-        } else {
-            this.triggeredState = value;
-        }
-        this.otherService.getCharacteristic(Characteristic.LockCurrentState).updateValue(this.armedtState);
+Alarm.prototype.alarmTriggered = function (value) {
+    if (value >= this.alarmsystem_trigger && this.targetState != Characteristic.SecuritySystemCurrentState.ALARM_TRIGGERED) {
+        this.otherService.updateCharacteristic(this.homebridge.hap.Characteristic.SecuritySystemCurrentState, 4);
+    }
+}
+
+Alarm.prototype.callBack = function (value) {
+    if (value == 0) {
+        this.otherService.updateCharacteristic(this.homebridge.hap.Characteristic.SecuritySystemTargetState, 0);
+        this.otherService.updateCharacteristic(this.homebridge.hap.Characteristic.SecuritySystemCurrentState, 0);
+    } else {
+        this.otherService.updateCharacteristic(this.homebridge.hap.Characteristic.SecuritySystemTargetState, 1);
+        this.otherService.updateCharacteristic(this.homebridge.hap.Characteristic.SecuritySystemCurrentState, 1);
     }
 
-    if (this.stateDisableMove == uuid) {
-        console.log("away mode " + value + " " + uuid);
-        this.moveDisabled = value;
-    }
-    if (this.stateUuid == uuid) {
-        console.log("state armed " + value + " " + uuid);
+    this.targetState = value;
+}
 
-        if (!value) {
-            this.armedtState = 3;
-        } else if (value && this.moveDisabled && this.triggeredState == 0) {
-            this.armedState = 0;
-        } else if (value && !this.moveDisabled && this.triggeredState == 0) {
-            this.armedState = 1;
-        } else if (this.triggeredState > 0) {
-            this.armedState = 4;
-        }
-
-        
-
-        if (this.armedtState == 3) {
-            this.targetState = 3;
-            this.otherService.setCharacteristic(Characteristic.SecuritySystemTargetState, Characteristic.SecuritySystemTargetState.DISARM);
-         this.setFromLoxone = true;
-        }
-        if (this.armedtState == 0) {
-            this.targetState = 0;
-            this.otherService.setCharacteristic(Characteristic.SecuritySystemTargetState, Characteristic.SecuritySystemTargetState.STAY_ARM);
-         this.setFromLoxone = true;
-        }
-        if (this.armedtState == 1) {
-            this.targetState = 1;
-            this.otherService.setCharacteristic(Characteristic.SecuritySystemTargetState, Characteristic.SecuritySystemTargetState.AWAY_ARM);
-         this.setFromLoxone = true;
-        }
-        this.otherService.getCharacteristic(Characteristic.LockCurrentState).updateValue(this.armedState)
-
-    }
-};
-
-Alarm.prototype.getOtherServices = function() {
+Alarm.prototype.getOtherServices = function () {
     var otherService = new this.homebridge.hap.Service.SecuritySystem();
-    otherService.getCharacteristic(this.homebridge.hap.Characteristic.SecuritySystemCurrentState)
-        .on("get", this.getCurrentState.bind(this));
-    
 
     otherService.getCharacteristic(this.homebridge.hap.Characteristic.SecuritySystemTargetState)
-        .on('set', this.setItemTargetState.bind(this))
-        .on('get', this.getItemTargetState.bind(this))
-
+        .setProps({
+            validValues: [0, 1]
+        })
+        .on('get', this.getTargetState.bind(this))
+        .on('set', this.setTargetState.bind(this));
 
     return otherService;
 };
 
-Alarm.prototype.getCurrentState = function(callback) {
-   callback(undefined, this.armedState);
+Alarm.prototype.getTargetState = function (callback) {
+    callback(null, this.targetState);
 };
 
-Alarm.prototype.getItemTargetState = function(callback) {
-   callback(undefined, this.targetState);
+Alarm.prototype.setTargetState = function (value, callback) {
+    this.updateTargetState(value, callback);
 };
 
-Alarm.prototype.getItemAlarmType = function(callback) {
-   callback(undefined, this.AlarmType);
-};
+Alarm.prototype.updateTargetState = function (state, callback) {
 
+    const command = (state == '1') ? this.alarmsystem_method : 'off';
 
-Alarm.prototype.onCommand = function() {
-    return 'On';
-};
+    this.log("[Alarm] HomeKit - send message to " + this.name + ": " + command);
+    this.platform.ws.sendCommand(this.uuidAction, command);
 
-Alarm.prototype.setItemTargetState = function(value, callback) {
-    //  this.log("Setting state to %s", value);
-    if (!this.setFromLoxone) {
-        var self = this;
-        this.targetState = value;
-        //var command = (value == '1') ? this.onCommand() : 'Off';
-        if (value == '0') {
-            var command = 'on/0';
-        } else if (value == '1') {
-            var command = 'on/1';
-        } else if (value == '3') {
-            var command = 'Off';
-        } else if (value == '2') {
-            var command = 'on/0';
-        }
-        // this.log("[Alarm] iOS - send message to " + this.name + ": " + command);
-        this.platform.ws.sendCommand(this.uuidAction, command);
-        if (command == 'Off') {
-            //this.log("[Alarm] iOS - send message to " + this.name + ": " + "quit");
-            this.platform.ws.sendCommand(this.uuidAction, 'quit');
-        }
-    } else {
-        this.setFromLoxone = false;
-    }
+    this.targetState = state;
 
     callback();
 };
