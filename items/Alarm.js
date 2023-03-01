c462520c193f555651b29bbac8c2438bf3c592c6
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-03-01 18:29:26 +0100

    Update Alarm.js

--- items/Alarm.js
+++ items/Alarm.js
@@ -1,43 +1,54 @@
-var Alarm = function (widget, platform, homebridge) {
+var Alarm = function(widget, platform, homebridge) {
     Characteristic = homebridge.hap.Characteristic;
 
     this.platform = platform;
     this.uuidAction = widget.uuidAction;
     this.stateUuid = widget.states.armed;
     this.stateUuidAlarm = widget.states.level;
+    this.stateMovementSensorsDisabled = widget.states.disabledMove;
     this.alarmsystem_method = platform.alarmsystem_method;
     this.alarmsystem_trigger = platform.alarmsystem_trigger;
 
     this.targetState = 0;
     this.alarmlevel = 0;
+    this.sensorDisabled = 0;
 
     Alarm.super_.call(this, widget, platform, homebridge);
 };
 
-Alarm.prototype.initListener = function () {
+Alarm.prototype.initListener = function() {
     this.platform.ws.registerListenerForUUID(this.stateUuid, this.callBack.bind(this));
     this.platform.ws.registerListenerForUUID(this.stateUuidAlarm, this.alarmTriggered.bind(this));
+    this.platform.ws.registerListenerForUUID(this.stateMovementSensorsDisabled, this.sensorsDisabled.bind(this));
 };
 
-Alarm.prototype.alarmTriggered = function (value) {
+Alarm.prototype.alarmTriggered = function(value) {
     if (value >= this.alarmsystem_trigger && this.targetState != Characteristic.SecuritySystemCurrentState.ALARM_TRIGGERED) {
         this.otherService.updateCharacteristic(this.homebridge.hap.Characteristic.SecuritySystemCurrentState, 4);
     }
 }
+Alarm.prototype.alarmTriggered = function(value) {
+    this.sensorDisabled = value;
+}
 
-Alarm.prototype.callBack = function (value) {
+Alarm.prototype.callBack = function(value) {
     if (value == 0) {
+        this.otherService.updateCharacteristic(this.homebridge.hap.Characteristic.SecuritySystemTargetState, 3);
+        this.otherService.updateCharacteristic(this.homebridge.hap.Characteristic.SecuritySystemCurrentState, 3);
+        this.targetState = 3;
+    } else if (this.sensorDisabled) {
         this.otherService.updateCharacteristic(this.homebridge.hap.Characteristic.SecuritySystemTargetState, 0);
         this.otherService.updateCharacteristic(this.homebridge.hap.Characteristic.SecuritySystemCurrentState, 0);
+        this.targetState = 0;
     } else {
         this.otherService.updateCharacteristic(this.homebridge.hap.Characteristic.SecuritySystemTargetState, 1);
         this.otherService.updateCharacteristic(this.homebridge.hap.Characteristic.SecuritySystemCurrentState, 1);
+        this.targetState = 1;
     }
 
-    this.targetState = value;
 }
 
-Alarm.prototype.getOtherServices = function () {
+Alarm.prototype.getOtherServices = function() {
     var otherService = new this.homebridge.hap.Service.SecuritySystem();
 
     otherService.getCharacteristic(this.homebridge.hap.Characteristic.SecuritySystemTargetState)
@@ -50,24 +61,43 @@ Alarm.prototype.getOtherServices = function () {
     return otherService;
 };
 
-Alarm.prototype.getTargetState = function (callback) {
+Alarm.prototype.getTargetState = function(callback) {
     callback(null, this.targetState);
 };
 
-Alarm.prototype.setTargetState = function (value, callback) {
+Alarm.prototype.setTargetState = function(value, callback) {
     this.updateTargetState(value, callback);
 };
 
-Alarm.prototype.updateTargetState = function (state, callback) {
+Alarm.prototype.updateTargetState = function(state, callback) {
+    /*
+        const command = (state == '1') ? this.alarmsystem_method : 'off';
 
-    const command = (state == '1') ? this.alarmsystem_method : 'off';
+        this.log("[Alarm] HomeKit - send message to " + this.name + ": " + command);
+        this.platform.ws.sendCommand(this.uuidAction, command);
 
-    this.log("[Alarm] HomeKit - send message to " + this.name + ": " + command);
+        this.targetState = state;
+
+        callback();
+        */
+
+    if (state == '0') {
+        var command = 'on/0';
+    } else if (state == '1') {
+        var command = 'on/1';
+    } else if (state == '3') {
+        var command = 'Off';
+    }
+
+    // this.log("[Alarm] iOS - send message to " + this.name + ": " + command);
     this.platform.ws.sendCommand(this.uuidAction, command);
-
-    this.targetState = state;
+    if (command == 'Off') {
+        //this.log("[Alarm] iOS - send message to " + this.name + ": " + "quit");
+        this.platform.ws.sendCommand(this.uuidAction, 'quit');
+    }
 
     callback();
+
 };
 
 module.exports = Alarm;
