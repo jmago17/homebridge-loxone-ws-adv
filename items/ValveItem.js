e2319b5263116c0f0f0acd85a0f03a6dbe979270
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-03-04 10:53:13 +0100

    Update ValveItem.js

--- items/ValveItem.js
+++ items/ValveItem.js
@@ -14,6 +14,7 @@ const ValveItem = function(widget, platform, homebridge) {
                 this.stateUuid = this.platform.Valves[item];
                 this.override = false;
                 this.daytimer = true;
+                console.log("programa termoooooooooooo");
 
             }
         }
