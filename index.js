689de6eb08b00023b2c65cb35d5145af66e555f7
jmago17 <32551505+jmago17@users.noreply.github.com>
2023-03-12 14:15:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let Accessory, Service, Characteristic, UUIDGen;
+let Accessory, Service, Characteristic, UUIDGen, FakeGatoHistoryService;
 const request = require("request");
 const ItemFactory = require('./libs/ItemFactory.js');
 const Utility = require('./libs/Utility.js');
