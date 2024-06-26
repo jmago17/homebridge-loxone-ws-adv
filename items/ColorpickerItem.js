8be817b63ffadb7ace286e6ea84372fbb25dbc50
jmago17 <32551505+jmago17@users.noreply.github.com>
2024-06-26 19:53:42 +0200

    Update ColorpickerItem.js

--- items/ColorpickerItem.js
+++ items/ColorpickerItem.js
@@ -409,10 +409,10 @@ ColorItem.prototype.setColorState = function(callback) {
 
     this.power = this.brightness > 0;
     }else {
-        var command = "hsv(" + this.hue + "," + this.saturation + "," + this.brightness + ")";
-    this.log("[color] iOS - send message to " + this.name + ": " + command);
-    this.platform.ws.sendCommand(this.uuidAction, command);
-    callback();    }
+        command = "temp(" + this.brightness + "," + homekitToLoxoneColorTemperature(this.colortemperature, this) + ")";
+        this.log("[color] iOS - send message to " + this.name + ": " + command);
+        this.platform.ws.sendCommand(this.uuidAction, command);
+        callback();    }
     
 };
 
